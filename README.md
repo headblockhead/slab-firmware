cbae8c5af427ddbc64f7a62fcceb3e4a56df8f56
Edward Hesketh <inbox@edwardh.dev>
2026-05-31 17:53:18 +0100

    feat: blinky blinky via PIO

--- README.md
+++ README.md
@@ -18,3 +18,12 @@ cmake --build . -j $(nproc)
 rm -rf build
 mkdir build
 ```
+
+### Upload
+
+Requires: build
+Directory: build
+
+```bash
+cp ./prototype/prototype.uf2 $(findmnt -S LABEL=RPI-RP2 -o TARGET -fn)
+```
--- prototype/main.c
+++ prototype/main.c
@@ -1,8 +1,25 @@
+#include "edbus.pio.h"
+#include "hardware/pio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
 
 int main() {
-  stdio_init_all();
-  printf("Hello, world!\n");
-  return 0;
+  PIO pio;
+  uint sm;
+  uint offset;
+
+  bool success = pio_claim_free_sm_and_add_program_for_gpio_range(
+      &edbus_program, &pio, &sm, &offset, 25, 1, true);
+  hard_assert(success);
+
+  edbus_program_init(pio, sm, offset, 25);
+
+  while (1) {
+    pio_sm_put_blocking(pio, sm, 1);
+    sleep_ms(500);
+    pio_sm_put_blocking(pio, sm, 0);
+    sleep_ms(500);
+  }
+
+  pio_remove_program_and_unclaim_sm(&edbus_program, pio, sm, offset);
 }
